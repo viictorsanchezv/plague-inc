82bb948abaab11c62a09951e314027fadf10c1c3
viictorsanchezv <ingsanchez423@gmail.com>
2021-08-08 10:56:11 -0400

    calculator people event

--- app/Http/Livewire/Map.php
+++ app/Http/Livewire/Map.php
@@ -7,16 +7,55 @@ use Livewire\Component;
 class Map extends Component
 {
     public $civil, $militar, $gobierno, $postCount;
-
+    public $poblacion_neutral=34174, $poblacion_a_favor=9150;
+    public $estabilidad;
+    public $reputacion=90;
+    public $pobl_insurgente=2000;
     public function render()
     {
         return view('livewire.map');
     }
 
-    protected $listeners = ['postAdded' => 'incrementPostCount'];
+    protected $listeners = ['postAdded'     => 'incrementPostCount',
+                            'cal_poblacion' => 'cal_poblacion'];
 
     public function incrementPostCount()
-    {
+    {   
+        $band = false;
+        $random = rand( 0, 100);
+        if($random == 10){
+            $band = true;
+            if($this->poblacion_neutral > 10){
+                $this->poblacion_neutral -= 10;
+                $this->pobl_insurgente +=10;
+            }
+            
+        }else{
+            $this->cal_evento($random);
+        }
         $this->postCount += 1;
+    }
+
+    public function cal_poblacion(){
+        if($this->reputacion > 20 ){
+            if($this->poblacion_neutral > 3){
+                $this->poblacion_a_favor +=3;
+                $this->poblacion_neutral -= 3;
+            } 
+        }
+    }
+    public function cal_evento($rand){
+        if($rand > 0 && $rand < 10){
+
+        }else{
+            if($rand >= 10 && $rand < 15){
+
+            }else{
+                if($rand >= 15 && $rand < 20){
+
+                }
+            }
+        }
+
     }
 }
--- resources/views/livewire/map.blade.php
+++ resources/views/livewire/map.blade.php
@@ -4,13 +4,13 @@
         <div class='w-full flex'>
             <div class='bg-yellow-100 rounded-lg mt-2 w-7/12 mx-auto flex justify-around'>
                 <div class='bg-yelloy-300 text-xl text-black'>
-                    747
+                    {{ $poblacion_a_favor }}
                 </div>
                 <div class='bg-yelloy-300 text-xl text-black'>
-                    Estabilidad 2%
+                    Estabilidad {{ $estabilidad }} %
                 </div>
                 <div class='bg-yelloy-300 text-xl text-black'>
-                    23,258
+                    {{ $poblacion_neutral }}
                 </div>
             </div>
         </div>
@@ -303,6 +303,7 @@ function reassignDialog() {
     setInterval(() => {
         console.log("tick");
         Livewire.emit('postAdded');
-    }, 1000);
+        Livewire.emit('cal_poblacion');
+    }, 2000);
     //Livewire.emit('postAdded');
 </script>
